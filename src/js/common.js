1864f6f0804c311531d1aedaef0ce81e09edd625
Harvey Zack <laozhaochaguan@hotmail.com>
2018-09-19 20:26:16 +0800

    update

--- src/js/common.js
+++ src/js/common.js
@@ -2,7 +2,7 @@ import Highway from "@dogstudio/highway/build/es5/highway";
 
 const H = new Highway.Core({
   renderers: {
-    index: import( /* webpackChunkName: "index" */ './index'),
-    about: import( /* webpackChunkName: "about" */ './about')
+    index: () => import( /* webpackChunkName: "index" */ './index'),
+    about: () => import( /* webpackChunkName: "about" */ './about')
   }
 });
