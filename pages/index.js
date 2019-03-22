4f4712f377ad897e785c0a0dab98f2774e879708
Harvey Zack <laozhaochaguan@gmail.com>
2019-03-22 16:05:17 +0800

    next.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+// pages/index.js
+import Link from 'next/link'
+
+export default () =>
+  <div>
+    Click{' '}
+    <Link href={{ pathname: '/about', query: { name: 'Zeit' }}}>
+      <a>here</a>
+    </Link>{' '}
+    to read more
+  </div>
