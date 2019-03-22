2dbe0327e3c46acffdf713c0775056e1ca735ac7
Harvey Zack <laozhaochaguan@gmail.com>
2019-03-22 14:30:56 +0800

    react

--- src/js/common.js
+++
@@ -1,8 +0,0 @@
-import Highway from "@dogstudio/highway";
-
-const H = new Highway.Core({
-  renderers: {
-    index: () => import( /* webpackChunkName: "index" */ './index'),
-    about: () => import( /* webpackChunkName: "about" */ './about')
-  }
-});
---
+++ tmp/dev-server/react-static-browser-plugins.js
@@ -0,0 +1,12 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/zhaohaiwei/Desktop/github/sleepy",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
