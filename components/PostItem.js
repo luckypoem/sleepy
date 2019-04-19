6e841f73ed4b6bd4312105d4898e0efef3c8908f
Harvey Zack <laozhaochaguan@gmail.com>
2019-04-19 10:46:39 +0800

    update

--- components/PostItem.js
+++ components/PostItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Router from 'next/router';
 import Link from 'next/link';
-import { inject, observer } from 'mobx-react';
+import mediumZoom from 'medium-zoom';
 import api from '../api';
 import Title from './Title';
 import Gitting from '../components/Gitting';
@@ -10,8 +10,6 @@ import Navigation from '../components/Navigation';
 import { seo } from '../sleepy.config';
 import { relative, format, searchParams, getNavs, t } from '../utils';
 
-@inject('store')
-@observer
 export default class extends React.Component {
   constructor(props) {
     super(props);
@@ -30,12 +28,28 @@ export default class extends React.Component {
         .then(data => {
           this.setState(() => ({
             id: id,
-            post: data,
+            post: data
           }));
 
           this.setState(() => ({
             navigation: getNavs(this.htmlRef.current)
           }));
+
+          [...this.htmlRef.current.querySelectorAll('a')]
+            .filter(item => {
+              return (
+                item.children.length === 1 && item.children[0].tagName === 'IMG'
+              );
+            })
+            .map(item => {
+              item.href = 'javascript:void(0);';
+              return item.children[0];
+            })
+            .forEach(item => {
+              mediumZoom(item, {
+                background: 'rgba(255, 255, 255, 0.8)',
+              });
+            });
         })
         .catch(() => {
           Router.push('/error');
@@ -76,10 +90,16 @@ export default class extends React.Component {
               dangerouslySetInnerHTML={{ __html: post.html }}
             />
           </div>
-          <div className="time" title={format(post.created_at)}>{relative(post.created_at)}</div>
+          <div className="time" title={format(post.created_at)}>
+            {relative(post.created_at)}
+          </div>
           <Navigation navigation={navigation} />
         </div>
-        {post.locked ? <div className="locked">{t('commentLocked')}</div> : <Gitting id={this.state.id} />}
+        {post.locked ? (
+          <div className="locked">{t('commentLocked')}</div>
+        ) : (
+          <Gitting id={this.state.id} />
+        )}
       </div>
     );
   }
