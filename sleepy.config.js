ba29586c9c5d266e03015debe4d320145bb81f8c
luckypoem <luckypoem@gmail.com>
2024-12-01 19:41:45 -0800

    Update sleepy.config.js

--- sleepy.config.js
+++ sleepy.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     seo: {
-        title: "老赵茶馆",
-        keywords: "老赵, 茶馆, 前端, 代码, 扯淡, 音乐, 电影, 分享",
+        title: "yu的茶馆",
+        keywords: "老yu, 茶馆, 前端, 代码, 扯淡, 音乐, 电影, 分享",
         description: "Hi, 老赵其实不老, 是一枚前端攻城狮, 就这样. QQ群：320881312",
         copyright: '© 2019 All Rights Reserved. Theme by Sleepy.'
     },
